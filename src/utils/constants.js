6dc47058c6401ff769584abbbd25df49d89ed931
Gopal-19 <60785555+Gopal-19@users.noreply.github.com>
2025-08-02 19:41:53 +0530

    constant file

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+export const LOGO = "https://img.hotstar.com/image/upload/f_auto,q_90,w_384/v1737554969/web-assets/prod/images/rebrand/logo.png";
+
+export const PHOTO_URL = "https://www.shutterstock.com/image-illustration/lo%E2%80%A6tton-glossy-reflected-symbol-260nw-2315277075.jpg";
+
+export const BACKGROUND_IMAGE = "https://secure-media.hotstar.com/static/sunset/us/assets/web/background-2.png";
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTQ4ZmViNGIwNjIzZDNkZmMyY2JiNDFiNzVmZTMzOCIsIm5iZiI6MTc0NTU4Mjc0MS41NTMsInN1YiI6IjY4MGI3YTk1ZTkyZjk0MGNhNjlkNTE2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oUOxtXpl6tjU455pGbDBVrbjf5-3ze9d8Hz1UZHbSCk'
+  }
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w300"
+
+export const SUPPORTED_LANGUAGE = [
+  { identifier: "en", name:"English" },
+  { identifier: "hindi", name:"Hindi" }
+];
+
+export const GEMENI_API_KEY = 'AIzaSyDPs1_qsbqCEgaYOA3nlhkP8klZNagHO1g';
+
